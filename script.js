297068875d6643fed3831d3df75fb1f033ba2fa7
jolley34 <erlandssonjoel@gmail.com>
2023-10-25 19:00:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".menu");
+const item = document.querySelectorAll(".item");
+const dropdown= document.querySelector(".dropdown");
+const closemenu= document.querySelector(".closemenu");
+const openmenu = document.querySelector(".openmenu");
+
+function toggleMenu() {
+  if (menu.classList.contains("showdropdown")) {
+    menu.classList.remove("showdropdown");
+    closemenu.style.display = "none";
+    openmenu.style.display = "block";
+  } else {
+    menu.classList.add("showdropdown");
+    closemenu.style.display = "block";
+    openmenu.style.display = "none";
+  }
+}
+
+dropdown.addEventListener("click", toggleMenu);
